d5d0016b6aafb9ebbcb860a06360307d2fb67036
Jose Alberto Solis <josealbertosolis@MacBook-Pro-de-Jose.local>
2021-06-28 11:53:28 +0200

    first commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,16 +1,8 @@
 import firebase from 'firebase'
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDt-zVUB-pn6KLt1INtPWlYgloqNSIV4fY",
-  authDomain: "proyecto-daw-eba1d.firebaseapp.com",
-  databaseURL: "https://proyecto-daw-eba1d.firebaseio.com",
-  projectId: "proyecto-daw-eba1d",
-  storageBucket: "proyecto-daw-eba1d.appspot.com",
-  messagingSenderId: "503936455919",
-  appId: "1:503936455919:web:488d8b93951bd6ff6d771a"
-};
+import { firebaseConfig } from 'userCredentials'
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
 const auth = firebase.auth()
-const db = firebase.database();
+const db = firebase.database()
 export { auth, db }
